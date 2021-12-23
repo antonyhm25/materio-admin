96eee5a581a2dc45e1df1c70029677e2428404bf
Antony HM <antonio_hm25@hotmail.com>
2021-12-22 22:25:31 -0600

    agregar modelo, factory y archivo de migracion para restaurantes

---
+++ database/factories/RestaurantFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RestaurantFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name'=> $this->faker->unique->company(),
+            'address'=> $this->faker->address(),
+            'photo'=> $this->faker->image(),
+        ];
+    }
+}
